749981be4ad0d975ade0871aab06cdeb2c0b8826
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-12-12 22:51:50 +0600

    json and dist folder created

--- first.ts
+++ first.ts
@@ -1,3 +1,18 @@
-let myMoney = 50;
-myMoney = 150;
-myMoney = 0;
+let age: number = 50;
+let club: string = "RM";
+const isFamous: boolean = false;
+let famous: boolean;
+
+function add(num1: any, num2: number | string) {
+  return num1 + num2;
+}
+
+add(1, 2);
+add("3", "adam");
+
+function fullName(firstName: string, lastName: string): string {
+  return firstName + " " + lastName;
+}
+
+const user: string = fullName("Taufiqul", "Islam");
+console.log(user);
