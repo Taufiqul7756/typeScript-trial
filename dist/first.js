05bcf0a58c3a0cd5f5e673e952b4b7e2bdbb2c18
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-12-15 15:21:51 +0600

    Array

--- dist/first.js
+++ dist/first.js
@@ -1,4 +1,31 @@
 "use strict";
-let myMoney = 50;
-myMoney = 150;
-myMoney = 0;
+// "use strict";
+// let age = 50;
+// let club = "RM";
+// const isFamous = false;
+// let famous;
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// const adding = add(1, 2);
+// console.log(adding);
+// const adding2 = add("3", "adam");
+// console.log(adding2);
+// function fullName(firstName, lastName) {
+//   return firstName + " " + lastName;
+// }
+// const user = fullName("Taufiqul", "Islam");
+// console.log(user);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.push(10);
+numbers.push(11);
+console.log(numbers);
+const friends = ["taufiqul", "george"];
+let megaName = " ";
+for (let i = 0; i < friends.length; i++) {
+    const friend = friends[i];
+    if (friend.length > megaName.length) {
+        megaName = friend;
+    }
+}
+console.log("Friends largest Name:", megaName);
--- first.ts
+++ first.ts
@@ -1,18 +1,32 @@
-let age: number = 50;
-let club: string = "RM";
-const isFamous: boolean = false;
-let famous: boolean;
+// "use strict";
+// let age = 50;
+// let club = "RM";
+// const isFamous = false;
+// let famous;
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// const adding = add(1, 2);
+// console.log(adding);
+// const adding2 = add("3", "adam");
+// console.log(adding2);
+// function fullName(firstName, lastName) {
+//   return firstName + " " + lastName;
+// }
+// const user = fullName("Taufiqul", "Islam");
+// console.log(user);
 
-function add(num1: any, num2: number | string) {
-  return num1 + num2;
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.push(10);
+numbers.push(11);
+console.log(numbers);
+
+const friends: string[] = ["taufiqul", "george"];
+let megaName: string = " ";
+for (let i = 0; i < friends.length; i++) {
+  const friend: string = friends[i];
+  if (friend.length > megaName.length) {
+    megaName = friend;
+  }
 }
-
-add(1, 2);
-add("3", "adam");
-
-function fullName(firstName: string, lastName: string): string {
-  return firstName + " " + lastName;
-}
-
-const user: string = fullName("Taufiqul", "Islam");
-console.log(user);
+console.log("Friends largest Name:", megaName);
