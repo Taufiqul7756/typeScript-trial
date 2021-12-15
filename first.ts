9446681b9de1c2898474fb4b65fe112de7b84e4a
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2021-12-15 23:22:40 +0600

    Object

--- first.ts
+++ first.ts
@@ -16,17 +16,34 @@
 // const user = fullName("Taufiqul", "Islam");
 // console.log(user);
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-numbers.push(10);
-numbers.push(11);
-console.log(numbers);
+// Array
 
-const friends: string[] = ["taufiqul", "george"];
-let megaName: string = " ";
-for (let i = 0; i < friends.length; i++) {
-  const friend: string = friends[i];
-  if (friend.length > megaName.length) {
-    megaName = friend;
-  }
-}
-console.log("Friends largest Name:", megaName);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// numbers.push(10);
+// numbers.push(11);
+// console.log(numbers);
+
+// const friends: string[] = ["taufiqul", "george"];
+// let megaName: string = " ";
+// for (let i = 0; i < friends.length; i++) {
+//   const friend: string = friends[i];
+//   if (friend.length > megaName.length) {
+//     megaName = friend;
+//   }
+// }
+// console.log("Friends largest Name:", megaName);
+
+//object
+const friend: { name: string; age: number } = {
+  name: "samuel David",
+  age: 61,
+};
+friend.age = 57;
+
+let player: {
+  club: string;
+  salary: number;
+} = {
+  club: "Real Madrid",
+  salary: 450000,
+};
